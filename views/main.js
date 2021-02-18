87c04c2cb03b2771f8d17379099697d3e3e47fa1
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2021-02-18 23:48:10 +0200

    add missing fn to client main script

--- views/main.js
+++ views/main.js
@@ -110,3 +110,24 @@ if ('Notification' in window){
     const enableNotificationsBtn = document.getElementById('enable-notifications');
     enableNotificationsBtn.style.display = 'none';
 }
+
+
+/**
+ * Converts a base64 string to a Uint8Array
+ * @param {string} base64String a public vapid key
+ * @returns {Uint8Array}
+ */
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
