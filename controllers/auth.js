16ace7fddde71b053b63801b238ceb50410fee97
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2022-01-02 18:26:27 +0200

    refactor auth controller with async/await

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,118 +6,87 @@ const { __ } = require('i18n');
 
 const config = require('../config/config');
 const User = require('../models/user');
+const asyncMiddleware = require('../middleware/async');
+const { throwError } = require('../utils/throwError');
 
-const login = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error(__('Validation error occured'));
-    error.statusCode = 422;
-    error.data = { errors: errors.array() };
-    throw error;
-  }
-
-  const { email } = req.body;
-  const { password } = req.body;
-
-  let loadedUser = null;
-
-  User.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        const error = new Error(
-          __('A user with this email could not be found')
-        );
-        error.statusCode = 401;
-        error.data = { email };
-        throw error;
-      }
-      if (!user.active) {
-        const error = new Error(__('User is not active'));
-        error.statusCode = 401;
-        error.data = { userId: user._id.toString() };
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((isEqual) => {
-      if (!isEqual) {
-        const error = new Error(__('Wrong password!'));
-        error.statusCode = 401;
-        throw error;
-      }
-      const token = jwt.sign(
-        { email: loadedUser.email, userId: loadedUser._id.toString() },
-        config.jwt.secret,
-        { expiresIn: config.jwt.expires }
-      );
-      res.status(200).json({
-        ok: true,
-        message: __('Login ok'),
-        data: {
-          userId: loadedUser._id.toString(),
-          userEmail: loadedUser.email,
-          website: loadedUser.website,
-          token,
-          vapidPublicKey: loadedUser.vapidKeys.publicKey,
-        },
-      });
-    })
-    .catch((err) => {
-      next(err);
-    });
-};
-
-const signup = (req, res, next) => {
+const login = asyncMiddleware(async (req, res, next) => {
+  const { email, password } = req.body;
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    const error = new Error(__('Validation error occured'));
-    error.statusCode = 422;
-    error.data = { errors: errors.array() };
-    throw error;
+    throwError('Validation error occured', 422, { errors: errors.array() });
   }
 
-  const { name } = req.body;
-  const { email } = req.body;
-  const { password } = req.body;
-  const website = req.body.website || '';
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throwError('A user with this email could not be found', 401, { email });
+  }
+
+  if (!user.active) {
+    throwError('User is not active', 401, { userId: user._id.toString() });
+  }
+
+  const isPasswordEqual = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordEqual) {
+    throwError('Wrong password!', 401);
+  }
+
+  const token = jwt.sign(
+    { email: user.email, userId: user._id.toString() },
+    config.jwt.secret,
+    { expiresIn: config.jwt.expires }
+  );
+
+  res.status(200).json({
+    ok: true,
+    message: __('Login ok'),
+    data: {
+      userId: user._id.toString(),
+      userEmail: user.email,
+      website: user.website,
+      token,
+      vapidPublicKey: user.vapidKeys.publicKey,
+    },
+  });
+});
+
+const signup = asyncMiddleware(async (req, res, next) => {
+  const { name, email, password, website = '' } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throwError('Validation error occured', 422, { errors: errors.array() });
+  }
 
   const vapidKeys = webpush.generateVAPIDKeys();
 
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPassword) => {
-      const user = new User({
-        name,
-        email,
-        website,
-        vapidKeys,
-        active: false,
-        password: hashedPassword,
-      });
-
-      user
-        .save()
-        .then((_result) => {
-          res.status(201).json({
-            ok: true,
-            message: __('Account created!'),
-            data: {
-              userId: user._id.toString(),
-              email: user.email,
-              vapidPublicKey: user.vapidKeys.publicKey,
-            },
-          });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    })
-    .catch((err) => {
-      next(err);
+  const hashedPassword = await bcrypt.hash(password, 12);
+  if (hashedPassword) {
+    const user = new User({
+      name,
+      email,
+      website,
+      vapidKeys,
+      active: false,
+      password: hashedPassword,
     });
-};
+
+    const userSaved = await user.save();
+    if (userSaved) {
+      res.status(201).json({
+        ok: true,
+        message: __('Account created!'),
+        data: {
+          userId: user._id.toString(),
+          email: user.email,
+          vapidPublicKey: user.vapidKeys.publicKey,
+        },
+      });
+    }
+  }
+});
 
 module.exports = {
   login,
